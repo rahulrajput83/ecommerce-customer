874084c961e53da29fee8d5ceb9ad743d973b6a3
Rahul Rajput <rahulrajput21112@gmail.com>
2023-03-04 17:11:19 +0530

    Cart btn

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { useRouter } from "next/router"
-import { BiHomeAlt2, BiLogIn, BiSearch, BiUser } from 'react-icons/bi'
+import { BiCart, BiHomeAlt2, BiLogIn, BiSearch, BiUser } from 'react-icons/bi'
 
 export default function Navbar() {
     const router = useRouter();
@@ -13,14 +13,19 @@ export default function Navbar() {
         router.push('/login')
     }
     return (
-        <div className="flex flex-row fixed z-10 w-full justify-center items-center bg-red-500 p-3 box-border">
+        <div className="flex flex-row fixed z-10 gap-2 sm:gap-5 w-full justify-center items-center bg-red-500 p-3 box-border">
             <Link href='/' className="text-white hidden sm:block font-semibold mr-auto text-base"><span className="font-bold text-lg">EASY</span> ORDER</Link>
-            <form className="flex mr-2 sm:mr-10 flex-row relative w-full sm:w-2/3 md:w-1/3 justify-center items-center">
+            <form className="flex flex-row relative w-full sm:w-2/3 md:w-1/3 justify-center items-center">
                 <input className="w-full py-2 rounded-full bg-white outline-none text-sm px-3" type='text' placeholder="Search" />
                 <button type="submit" className="bg-red-200 rounded-full hover:bg-red-100 cursor-pointer absolute flex w-10 justify-center items-center right-0 h-full">
                     <BiSearch className="" />
                 </button>
             </form>
+
+            <Link href='/cart' className='p-2 sm:py-1 sm:px-3 flex flex-row justify-center items-center gap-1 rounded-full border-2 border-white hover:bg-red-500  hover:text-white uppercase font-semibold bg-white'>
+                <BiCart className="text-xl " />
+                <span className="text-sm hidden sm:block">Cart</span>
+            </Link>
 
             <button onClick={handlebtn} className='p-2 sm:py-1 sm:px-3 flex flex-row justify-center items-center gap-1 rounded-full border-2 border-white hover:bg-red-500  hover:text-white uppercase font-semibold bg-white'>
                 {router.pathname.includes('login') ?
@@ -35,7 +40,6 @@ export default function Navbar() {
                         <span className="text-sm hidden sm:block">Login</span>
                     </>}
             </button>
-
         </div>
     )
 }
