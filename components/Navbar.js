de6595daea98cba4edba8494ad5747adfafc4537
Rahul Rajput <rahulrajput21112@gmail.com>
2023-03-04 16:29:29 +0530

    Responsive Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { useRouter } from "next/router"
-import { BiHomeAlt2, BiLogIn, BiSearch } from 'react-icons/bi'
+import { BiHomeAlt2, BiLogIn, BiSearch, BiUser } from 'react-icons/bi'
 
 export default function Navbar() {
     const router = useRouter();
@@ -15,14 +15,14 @@ export default function Navbar() {
     return (
         <div className="flex flex-row fixed z-10 w-full justify-center items-center bg-red-500 p-3 box-border">
             <Link href='/' className="text-white font-semibold mr-auto text-base"><span className="font-bold text-lg">EASY</span> ORDER</Link>
-            <form className="flex mr-10 flex-row relative w-1/3 justify-center items-center">
+            <form className="flex mr-2 sm:mr-10 flex-row relative w-full sm:w-2/3 md:w-1/3 justify-center items-center">
                 <input className="w-full py-2 rounded-full bg-white outline-none text-sm px-3" type='text' placeholder="Search" />
                 <button type="submit" className="bg-red-200 hover:bg-red-100 cursor-pointer absolute flex w-10 justify-center items-center right-0 h-full">
                     <BiSearch className="" />
                 </button>
             </form>
 
-            <button onClick={handlebtn} className='py-1 px-3 flex flex-row justify-center items-center gap-1 rounded-full border-2 border-white hover:bg-red-500  hover:text-white uppercase font-semibold bg-white'>
+            <button onClick={handlebtn} className='p-2 sm:py-1 sm:px-3 flex flex-row justify-center items-center gap-1 rounded-full border-2 border-white hover:bg-red-500  hover:text-white uppercase font-semibold bg-white'>
                 {router.pathname.includes('login') ?
                     <>
                         <BiHomeAlt2 className="text-xl" />
@@ -30,8 +30,9 @@ export default function Navbar() {
                     </>
                     :
                     <>
-                        <BiLogIn className="text-xl" />
-                        <span className="text-sm">Login</span>
+                        <BiUser className="block text-xl sm:hidden" />
+                        <BiLogIn className="text-xl hidden sm:block" />
+                        <span className="text-sm hidden sm:block">Login</span>
                     </>}
             </button>
 
