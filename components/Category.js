53d4e4fb4901f4b5f697cec321205eeb278b0338
Rahul Rajput <rahulrajput21112@gmail.com>
2023-03-07 22:02:09 +0530

    Added Responsive for Filter

--- components/Category.js
+++ components/Category.js
@@ -1,17 +1,17 @@
 import { useState } from "react";
 import { BiChevronDown } from "react-icons/bi";
 
-export default function Category({data, title, filter, setFilter}) {
+export default function Category({ data, title, filter, setFilter, showFilter }) {
     const [showComponent, setShowComponent] = useState(false)
     return (
-        <div className='w-full mt-2 flex flex-col'>
-            <div onClick={() => setShowComponent(!showComponent)} className='cursor-pointer px-2 w-full flex justify-between items-center'>
+        <div className={`w-full ${showFilter ? 'flex': 'hidden'} md:flex flex-col`}>
+            <div onClick={() => setShowComponent(!showComponent)} className='cursor-pointer px-2 py-1 w-full flex justify-between items-center'>
                 <span className="text-sm">{title}</span>
                 <BiChevronDown />
             </div>
             <span className='px-4 font-normal text-sm'>{filter[title]}</span>
 
-            <div className={`w-full gap-1 flex-col flex transition-all duration-1000 ${showComponent ? ' h-auto' : 'h-0'} shadow-2xl z-10 p-2`}>
+            <div className={`w-full gap-1 px-4 py-1 flex-col flex transition-all duration-1000 ${showComponent ? ' h-auto' : 'h-0'} shadow-2xl z-10`}>
                 {data.map((e, i) => {
                     return (
                         showComponent && <span onClick={() => { setFilter({ ...filter, [title]: e }); setShowComponent(false) }} key={`${title}-${i}`} className='font-normal text-sm cursor-pointer'>{e}</span>
--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import Head from 'next/head'
 import { useEffect, useState } from 'react'
 import { localSave } from '@/Functions/LocalStorage'
 import Category from '@/components/Category'
+import { useWindowSize } from '@/Functions/GetWidth'
 
 const categories = ['Electronics', 'Footwear', 'Home, Kitchen, Pets', 'Beauty, Health, Grocery', 'Books', "Men's Fashion", "Women's Fashion", "Kid's Fashion"]
 const price = ['Under ₹1,000', '₹1,000 - ₹5,000', '₹5,000 - ₹10,000', 'Over ₹10,000']
@@ -20,6 +21,8 @@ export default function Home() {
     Price: '',
     productOrder: ''
   })
+  const size = useWindowSize();
+  const [showFilter, setShowFilter] = useState(false)
 
   useEffect(() => {
     const localCart = JSON.parse(localStorage.getItem('cart')) || [];
@@ -69,9 +72,10 @@ export default function Home() {
   }, [cart])
 
   useEffect(() => {
+
     console.log(filter)
   }, [filter])
-  
+
   return (
     <>
       <Head>
@@ -84,18 +88,20 @@ export default function Home() {
       <main className='w-100 flex flex-col box-border'>
         <Navbar cart={cart} setSearchQuery={setSearchQuery} />
         <div className='w-full grid px-2 md:px-4 gap-4 pb-10 mt-24 md:mt-20 grid-cols-1 md:grid-cols-5'>
-          <div className='md:col-span-1 font-medium flex flex-col'>
-            <div className='text-lg'>Filters</div>
-            <div className='w-full flex flex-col'>
-              <Category data={categories} title='Category' filter={filter} setFilter={setFilter} />
-              <Category data={price} title='Price' filter={filter} setFilter={setFilter} />
-              <div className='flex w-full gap-2 mt-4'>
-                {productOrder.map((e, i) => {
-                  return(
-                    <span onClick={() => setFilter({...filter, productOrder: e})} className={`py-1 px-2 border-2 border-red-500  rounded-full text-sm text-red-500 cursor-pointer ${filter.productOrder === e ? 'bg-red-500 text-white' : ''}`} key={`productOrder${i}`}>{e}</span>
-                  )
-                })}
-              </div>
+          <div className='md:col-span-1 gap-2 font-medium flex flex-col'>
+            <span onClick={() => {
+              size.width <= 768 ?
+              setShowFilter(!showFilter) :
+              console.log(size.width)
+            }} className='text-lg bg-red-500 text-white px-3 py-1 md:p-0 md:text-black md:bg-transparent md:rounded-none rounded-full ml-auto md:ml-0'>Filters</span>
+            <Category showFilter={showFilter} data={categories} title='Category' filter={filter} setFilter={setFilter} />
+            <Category showFilter={showFilter} data={price} title='Price' filter={filter} setFilter={setFilter} />
+            <div className={` w-full gap-2 ${showFilter ? 'flex': 'hidden'} md:flex mt-4`}>
+              {productOrder.map((e, i) => {
+                return (
+                  <span onClick={() => setFilter({ ...filter, productOrder: e })} className={`py-1 px-2 border-2 border-red-500  rounded-full text-sm text-red-500 cursor-pointer ${filter.productOrder === e ? 'bg-red-500 text-white' : ''}`} key={`productOrder${i}`}>{e}</span>
+                )
+              })}
             </div>
           </div>
           <div className='w-full md:col-span-4  gap-5 gap-y-10 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
