e98b548fd06c41a0f97eb3d0f4f3b2a6701e92b6
Rahul Rajput <rahulrajput21112@gmail.com>
2023-03-18 15:49:40 +0530

    Updated Schema

--- Model/User.js
+++ Model/User.js
@@ -4,6 +4,9 @@ const Register = new mongoose.Schema({
     email: String,
     password: String,
     type: String,
+    mobileNumber: Number,
+    DeliveryAddress: String,
+    
 });
 
 const RegisterModel = mongoose.models.UserData || mongoose.model('UserData', Register);
