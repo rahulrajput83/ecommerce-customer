bf2849163ec6816903bfeae15995484dcaa09867
Rahul Rajput <rahulrajput21112@gmail.com>
2023-03-18 17:37:28 +0530

    Fixed font size

--- components/Input.js
+++ components/Input.js
@@ -4,7 +4,7 @@ function Input({ placeholder, name, value, data, setData, type }) {
         setData({ ...data, [e.target.name]: e.target.value })
     }
     return (
-        <input type={type} onChange={handleData} value={value} name={name} placeholder={placeholder} className="outline-none border-[0.14rem] border-red-500 rounded-full font-medium autofill:bg-red-500 px-3 py-2" />
+        <input type={type} onChange={handleData} value={value} name={name} placeholder={placeholder} className="outline-none border-[0.14rem] border-red-500 rounded-full font-medium autofill:bg-red-500 text-sm px-3 py-2" />
     )
 }
 
--- components/Label.js
+++ components/Label.js
@@ -4,7 +4,7 @@ function Label({text, data, setData}) {
         setData({ ...data, [e.target.name]: e.target.value })
     }
     return (
-        <label onClick={handleData} className="cursor-pointer w-fit flex justify-center items-center gap-2 font-medium accent-red-500">
+        <label onClick={handleData} className="cursor-pointer w-fit flex justify-center items-center gap-2 text-sm font-medium accent-red-500">
             <input className="cursor-pointer" type="radio" value={text} name="type" />
             <span>{text}</span>
         </label>
