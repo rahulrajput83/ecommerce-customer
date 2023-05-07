5d8545954397a72c6e9003c4625d680a2aa299e8
Rahul Rajput <rahulrajput21112@gmail.com>
2023-05-07 12:26:34 +0530

    fix card issue

--- pages/api/addToCart.js
+++ pages/api/addToCart.js
@@ -10,9 +10,9 @@ const handler = async (req, res) => {
     }
     try {
         await MongoDBConnect();
-        const productId = req.body.data.id;
+        const productId = req.body.data._id;
         const { id } = req.user;
-        const findProduct = await CartModel.findOne({ productId: productId, userId: id, paid: false });
+        const findProduct = await CartModel.findOne({ 'product._id': productId, userId: id, paid: false });
         if (findProduct !== null && findProduct !== undefined && Object.keys(findProduct).length >= 1) {
             let ciphertext = await CryptoJS.AES.encrypt(JSON.stringify({ message: "Already in cart..." }), process.env.JWT).toString();
             res.json({ message: 'Success', value: ciphertext })
