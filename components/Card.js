4092e064e6c2d074814e9f96c5764b051f0a1438
Rahul Rajput <rahulrajput21112@gmail.com>
2023-05-20 12:07:15 +0530

    Updated Product Cards

--- components/Card.js
+++ components/Card.js
@@ -7,11 +7,11 @@ export default function Card(props) {
     const { title, price, thumbnail, _id } = props.data;
 
     return (
-        <div className="flex group flex-col w-full">
+        <div className="flex hover:shadow-xl bg-white transition-all duration-700 rounded-md px-2 h-fit group flex-col w-full">
             <div className="flex flex-col w-full gap-2">
                 <Link href={`/product/${_id}`} className="w-full flex flex-col cursor-pointer gap-1">
                     <div className="w-full flex gap-2 justify-start  relative items-start">
-                        <img alt="" src={thumbnail} className="bg-cover  bg-slate-200 w-full h-auto" />
+                        <img alt="" src={thumbnail} className="w-full md:h-80 h-40 bg-no-repeat bg-cover" />
                         <div className="absolute right-0 bg-red-500 p-2 text-sm font-medium text-white">&#x20b9; {price}</div>
                     </div>
                     <div className="w-full truncate font-font-medium">{title}</div>
