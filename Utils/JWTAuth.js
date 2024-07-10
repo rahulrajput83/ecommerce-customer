15ae2987d22cfc33b8155fc6d09c633a44cd8efe
Rahul Rajput <rahul@techfino.in>
2024-07-10 19:46:06 +0530

    fixed auth

--- Utils/JWTAuth.js
+++ Utils/JWTAuth.js
@@ -2,18 +2,23 @@ const jwt = require('jsonwebtoken');
 
 function JWTAuth(handler) {
   return async (req, res) => {
-    const token = req.headers['token'];
-    jwt.verify(token, process.env.JWT, (err, decoded) => {
-      if (err) {
-        res.json({message: 'Unauthorized'})
-      }
-      if(decoded.type && decoded.type !== 'Customer') {
-        res.json({message: 'Unauthorized'})
-        return;
-      }
-      req.user = decoded;
-      return handler(req, res);
-    })
+    try{
+      const token = req.headers['token'];
+      jwt.verify(token, process.env.JWT, (err, decoded) => {
+        if (err) {
+          res.status(400).json({message: 'Unauthorized'})
+        }
+        if(decoded.type && decoded.type !== 'Customer') {
+          res.status(400).json({message: 'Unauthorized'})
+          return;
+        }
+        req.user = decoded;
+        return handler(req, res);
+      })
+    }
+    catch(error) {
+      res.status(400).json({ message: 'Unauthorized'})
+    }
 
   }
 }
